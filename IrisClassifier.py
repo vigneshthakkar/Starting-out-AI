365e2b2aa108ca59420e358fecd48b525f0d35fc
Vignesh Thakkar <vigneshthakkar@gmail.com>
2017-02-21 06:14:19 +0530

    Add files via upload

--- IrisClassifier.py
+++ IrisClassifier.py
@@ -1,64 +1,65 @@
-from sklearn import linear_model
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.neural_network import MLPClassifier
 import mysql.connector
-from sklearn.naive_bayes import GaussianNB
 
-cls={'Iris-setosa':1,'Iris-versicolor':2,'Iris-virginica':3}
-clsrev={1:'Iris-setosa',2:'Iria-versicolor',3:'Iria-virginica'}
+cls={'Iris-setosa':1, 'Iris-versicolor':2, 'Iris-virginica':3}
+clsrev={1:'Iris-setosa', 2:'Iris-versicolor', 3:'Iris-virginica'}
 
-def logisticregression(x,y):
-	lr=linear_model.LogisticRegression(solver='newton-cg', multi_class='multinomial')
-	lr.fit(x,y)
-	return lr
+def fetchdata():
+	db=mysql.connector.connect(user='root', password='Vmrkvv$2212', host='127.0.0.1', database='vignesh')
+	c=db.cursor()
+	c.execute('select * from iris')
+	return c.fetchall()
 
-def lineardiscriminantanalysis(x,y):
-	lda=LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto')
-	lda.fit(x,y)
-	return lda
+def train(x,y):
+	neural=MLPClassifier(hidden_layer_sizes=(4,2), max_iter=5000)
+	neural.fit(x,y)
+	return neural
 
-def quadraticdiscriminantanalysis(x,y):
-	qda=QuadraticDiscriminantAnalysis()
-	qda.fit(x,y)
-	return qda
+def test(neural,x,y):
+	i=1
+	correctcount,wrongcount=0,0
+	for xtest,ytest in zip(x,y):
+		ypredict=neural.predict([xtest])
+		if ypredict[0]==ytest: correctcount+=1
+		else: wrongcount+=1
+		i+=1
+		neural.partial_fit([xtest],[ytest])
+	print('Correct count :',correctcount)
+	print('Wrong count :',wrongcount)
+	return neural
+	
+def prediction(neural,x):
+	y=neural.predict(x)
+	return y
 
-def gaussiannaivebayes(x,y):
-	gnb=GaussianNB()
-	gnb.fit(x,y)
-	return gnb
-
-db=mysql.connector.connect(database='vignesh', user='root', password='Vmrkvv$2212', host='127.0.0.1')
-c=db.cursor()
-c.execute('select * from iris')
-data=c.fetchall()
+data=fetchdata()
 x,y=[],[]
-for each in data:
+traindata=data[:40]+data[50:90]+data[100:140]
+testdata=data[40:50]+data[90:100]+data[140:150]
+for row in traindata:
 	a=[]
 	for i in range(5):
-		if i==4: y.append(cls[each[i]])
-		else: a.append(each[i])
+		if i==4: y.append(cls[row[i]])
+		else: a.append(row[i])
 	x.append(a)
-print('Choose a method : ')
-print('1. Logistic Regression')
-print('2. Linear Discriminant Analysis')
-print('3. Quadratic Discriminant Analysis')
-print('4. Gaussian Naive Bayes')
-num=int(input('Enter your choice : '))
-if num==1: classifier=logisticregression(x,y)
-elif num==2: classifier=lineardiscriminantanalysis(x,y)
-elif num==3: classifier=quadraticdiscriminantanalysis(x,y)
-elif num==4: classifier=gaussiannaivebayes(x,y)
-char='y'
-while char=='y':
-	print('Enter the dimensions of the test iris flower...')
+neural=train(x,y)
+x,y=[],[]
+for row in testdata:
 	a=[]
-	z=float(input('Enter the sepal length : '))
-	a.append(z)
-	z=float(input('Enter the sepal width : '))
-	a.append(z)
-	z=float(input('Enter the petal length : '))
-	a.append(z)
-	z=float(input('Enter the petal width : '))
-	a.append(z)
-	x=[a]
-	print('The flower might be : ',clsrev[classifier.predict(x)[0]])
-	char=input('Test one more sample? (y/n) : ')
+	for i in range(5):
+		if i==4: y.append(cls[row[i]])
+		else: a.append(row[i])
+	x.append(a)
+neural=test(neural,x,y)
+ch='y'
+while ch=='y':
+	print('Enter the features of the flower to predict the class...')
+	z=[]
+	z.append(float(input('Enter the sepal length : ')))
+	z.append(float(input('Enter the sepal width : ')))
+	z.append(float(input('Enter the petal length : ')))
+	z.append(float(input('Enter the patel width : ')))
+	x=[z]
+	y=prediction(neural,x)
+	print('The flower might belong to',clsrev[y[0]])
+	ch=input('Predict the class of another flower? (y/n) : ')
